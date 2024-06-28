991a00fcefb7a75f19c444b149d018c54f86e966
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-06-28 18:56:47 +0200

    ok

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
 "use strict"
+// 1
+const bankAccount = {
+    ownerName: 'John',
+    accountNum: 99999999,
+    balance: 3000,
+    deposit(amountD) {
+        this.balance += amountD;
+        this.updateBalance();
+        alert('Your balance: ' + this.balance);
+    },
+    withdraw(amountW) {
+        if (amountW > this.balance) {
+            alert('There`s not so much money');
+        } else {
+            this.balance -= amountW;
+            this.updateBalance();
+            alert('Your balance: ' + this.balance);
+        }
+    },
+    updateBalance() {
+        document.getElementById('balance').textContent = this.balance;
+    }
+};
+
+function updateBalance() {
+    const added = parseFloat(document.getElementById('deposit').value);
+    const rested = parseFloat(document.getElementById('withdraw').value);
+    
+    if (!isNaN(added) && added > 0) {
+        bankAccount.deposit(added);
+    }
+    
+    if (!isNaN(rested) && rested > 0) {
+        bankAccount.withdraw(rested);
+    }
+    
+    document.getElementById('deposit').value = '';
+    document.getElementById('withdraw').value = '';
+}
 // 2
 function knowWeather(){
 const weather = {
@@ -29,4 +68,28 @@ if (mailValue === user.email && passValue === user.password){
 else{
     console.log(false);
 }
+};
+// 4
+const movie = {
+    title: 'JavaScript',
+    director: 'Peppa Pig',
+    year: 3048,
+    rating: null,
+    correcting(){
+       return this.rating > 8;
+    }
+};
+document.getElementById('title').innerText = 'Title:' + movie.title;
+document.getElementById('director').innerText = 'Director: ' + movie.director;
+document.getElementById('year').innerText = 'Year:' + movie.year;
+
+function movieUpdate(){
+movie.rating = parseFloat(document.getElementById('movieRating').value);
+document.getElementById('rating').innerText = 'Rating:' + movie.rating;
+
+    if (movie.correcting()=== true){
+        document.getElementById('title').classList.add('high-rating');
+    } else{
+        document.getElementById('title').classList.remove('high-rating'); 
+    }
 };
